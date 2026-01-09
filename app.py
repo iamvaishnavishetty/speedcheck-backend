a906bb612d070e08eefcffbb804ff01bcfc79e79
iamvaishnavishetty <72445335+iamvaishnavishetty@users.noreply.github.com>
2026-01-09 16:54:56 +0530

    Enable CORS and API response

--- app.py
+++ app.py
@@ -2,16 +2,16 @@ from flask import Flask, jsonify
 from flask_cors import CORS
 
 app = Flask(__name__)
-
-# ✅ ENABLE CORS (THIS IS THE MAIN FIX)
 CORS(app)
 
 @app.route("/")
 def home():
     return jsonify({
-        "message": "Backend is running successfully"
+        "message": "Backend is running successfully",
+        "download_speed": "92 Mbps",
+        "upload_speed": "41 Mbps",
+        "ping": "12 ms"
     })
 
 if __name__ == "__main__":
     app.run()
-
